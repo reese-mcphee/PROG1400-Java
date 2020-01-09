8cf23dfdf3514935e9d553b65382389d8f4a3f7c
Reese McPhee <w0241693@nscc.ca>
2020-01-09 12:18:04 -0400

    initial version

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+
+    class HelloWorld {
+
+        public static void main(String[] args) {
+
+            int num;
+
+            num = 123456;
+
+            System.out.println("Hello World!");
+
+            System.out.println("num =" + num);
+        }
+
+    }
