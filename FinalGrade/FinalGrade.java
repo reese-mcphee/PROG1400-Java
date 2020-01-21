797903af419149db3655caa856c8fda901aa3de1
Reese McPhee <43142985+reese-mcphee@users.noreply.github.com>
2020-01-21 14:47:49 -0400

    for loop

--- FinalGrade/FinalGrade.java
+++ FinalGrade/FinalGrade.java
@@ -14,13 +14,19 @@ public class FinalGrade {
         // loop 6 times to get all assignment grades
 
         float finalGrade = 0.0f;
+        float lowest = 100.0f;
 
         for (int i = 0; i<6; i++) {
             System.out.print("Enter grade for assignment " + (i+1) + ": ");
             assignment = s.nextFloat();
             finalGrade += assignment;
+
+            if (assignment < lowest) {
+                lowest = assignment;
+            }
         }
 
+        finalGrade -= lowest;
         finalGrade /= 5.0f;
 
         System.out.println("Your final grade is: " + finalGrade);
